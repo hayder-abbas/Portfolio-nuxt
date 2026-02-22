4493ae2a3b8d68f7b2de6d6b72ecbd95da929192
hayder-abbas <haydarabbas40@proton.me>
2026-02-22 17:33:46 +0300

    Install Tailwind CSS

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+	<div>
+		<NuxtRouteAnnouncer />
+		<h1 class="text-3xl font-bold underline">Welcome!!</h1>
+	</div>
 </template>
