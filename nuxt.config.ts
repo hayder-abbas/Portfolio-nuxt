e275d0ff75e4f63b8e0efadd1d186f03a35bb1d3
hayder-abbas <haydarabbas40@proton.me>
2026-03-05 23:23:21 +0300

    fit: Add Favicon for portfolio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,4 +11,19 @@ export default defineNuxtConfig({
       tailwindcss() as any,
     ],
   },
+  app: {
+    head: {
+      htmlAttrs: { dir: "ltr", lang: "en" },
+      link: [
+        { rel: "icon", type: "image/png", href: "/favicon/favicon-96x96.png", sizes: "96x96" },
+        { rel: "icon", type: "image/svg+xml", href: "/favicon/favicon.svg" },
+        { rel: "shortcut icon", href: "/favicon/favicon.ico" },
+        { rel: "apple-touch-icon", sizes: "180x180", href: "/favicon/apple-touch-icon.png" },
+        { rel: "manifest", href: "/favicon/site.webmanifest" },
+      ],
+      meta: [
+        { name: "apple-mobile-web-app-title", content: "HayderAbbas" },
+      ],
+    }
+  }
 });
